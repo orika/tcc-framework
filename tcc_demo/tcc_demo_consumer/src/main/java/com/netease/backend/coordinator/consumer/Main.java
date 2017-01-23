1c2dbf3f57f811f5625a528c2e845a209612f677
tanhua <tanhua@mogujie.com>
2017-01-23 14:41:20 +0800

    init

--- tcc_demo/tcc_demo_consumer/src/main/java/com/netease/backend/coordinator/consumer/Main.java
+++ tcc_demo/tcc_demo_consumer/src/main/java/com/netease/backend/coordinator/consumer/Main.java
@@ -1,8 +1,7 @@
 package com.netease.backend.coordinator.consumer;
 
 public class Main {
-	
-	
+
 	public static void main(String[] args) {
 		System.setProperty("dubbo.spring.config", "classpath*:/spring/*.xml");
 		com.alibaba.dubbo.container.Main.main(args);
